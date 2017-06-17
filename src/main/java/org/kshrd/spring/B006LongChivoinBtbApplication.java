7dfe3399471c5476083783b73bd7b048205b1a46
Long Chivoin <l.chivoin@gmail.com>
2017-06-18 01:55:01 +0700

    Initial commit

---
+++ src/main/java/org/kshrd/spring/B006LongChivoinBtbApplication.java
@@ -0,0 +1,12 @@
+package org.kshrd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B006LongChivoinBtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B006LongChivoinBtbApplication.class, args);
+	}
+}
---
+++ src/main/java/org/kshrd/spring/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.kshrd.spring;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(B006LongChivoinBtbApplication.class);
+	}
+
+}
