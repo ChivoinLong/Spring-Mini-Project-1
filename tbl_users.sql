91b680681e143a4b3bb273e45be01c6bd879abf8
Long Chivoin <l.chivoin@gmail.com>
2017-06-26 14:24:07 +0700

    Add files via upload
    added SQL script for table creation.

---
+++ tbl_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO "tbl_users" VALUES (1, 'Long Chivoin', 'Male', 'l.chivoin@gmail.com', '093385860', '75377fb0-f0f3-41fb-af4f-f29e9da4f65e', 'True');
+INSERT INTO "tbl_users" VALUES (2, 'Seng KimSeang', 'Male', 'kimseang@gmail.com', '098989898', '4ff30cb7-160a-4616-aeae-8bf550657bac', 'False');
+INSERT INTO "tbl_users" VALUES (3, 'Mom Reksmey', 'Female', 'mey@gmail.com', '021365846', '68bf1a77-cd20-4d46-ba2d-f230c426e1b1', 'True');
+INSERT INTO "tbl_users" VALUES (4, 'Tem Channat', 'Male', 'channat@gmail.com', '025875235', '15846e2a-e168-4376-8ef1-fb0c9a03f8b2', 'True');
+INSERT INTO "tbl_users" VALUES (5, 'Cheav Sovannarith', 'Male', 'rith@hotmail.com', '075239517', 'b9f28721-57a3-44a4-b532-bdf4fc8c3e57', 'True');
+INSERT INTO "tbl_users" VALUES (6, 'Yon Serey', 'Male', 'serey@gmail.com', '012345854', '6f120fbe-8229-4833-b519-bf255d7e557d', 'True');
